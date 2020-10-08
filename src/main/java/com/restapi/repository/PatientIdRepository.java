e11c348db40b44215b696d9ec0094678ce688a69
AnamikaGautam2018081 <kiran436gautam@gmail.com>
2020-10-08 10:55:21 +0000

    updated

--- src/main/java/com/restapi/repository/PatientIdRepository.java
+++ src/main/java/com/restapi/repository/PatientIdRepository.java
@@ -5,6 +5,9 @@ import com.restapi.model.PatientId;
 import org.springframework.data.jpa.repository.JpaRepository;
 //import org.springframework.data.mongodb.repository.MongoRepository;
 //import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface PatientIdRepository extends JpaRepository<PatientId, String>{
    // public storePid(Stirng pid)
     
